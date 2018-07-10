1127bd9aa2d4db869b17426798c63545f06e637d
wufang <fang.wu@viewfin.com>
2018-07-10 14:14:05 +0800

    tmp

--- src/main/java/xyz/sluggard/transmatch/controller/MatchController.java
+++ src/main/java/xyz/sluggard/transmatch/controller/MatchController.java
@@ -1,20 +1,41 @@
 package xyz.sluggard.transmatch.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import xyz.sluggard.transmatch.entity.Order;
 
 @RestController
 public class MatchController {
 	
-	public String newOrder() {
+	@Autowired
+	private String pair;
+	
+	@RequestMapping("/createOrder/{pair}")
+	public String newOrder(@PathVariable String pair,@RequestBody Order order) {
+		checkPair(pair);
+		
 		return null;
 	}
 	
-	public String canelOrder() {
+	@RequestMapping("/cancleOrder/{pair}/{orderId}")
+	public String canelOrder(@PathVariable String pair,@PathVariable String orderId) {
+		
 		return null;
 	}
 	
+	@RequestMapping("/getDepth/{pair}")
 	public String getOrderBook() {
 		return null;
+	}
+	
+	private void checkPair(String pair) {
+		if(!this.pair.equals(pair)) {
+			throw new IllegalArgumentException("error pair : " + pair);
+		}
 	}
 
 }
---
+++ src/main/java/xyz/sluggard/transmatch/vo/Order.java
@@ -0,0 +1,13 @@
+package xyz.sluggard.transmatch.vo;
+
+public class Order {
+	
+	private String userId;
+	
+	private String orderId;
+	
+	private int side;
+	
+	
+
+}
