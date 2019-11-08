ab1218b6863c0ab97fe2042c4e8c3e8e03472561
fang <sydra@163.com>
2019-11-08 11:05:34 +0800

    市价单撤单时，价格置零

--- src/main/java/xyz/sluggard/transmatch/event/CancelEvent.java
+++ src/main/java/xyz/sluggard/transmatch/event/CancelEvent.java
@@ -1,5 +1,7 @@
 package xyz.sluggard.transmatch.event;
 
+import java.math.BigDecimal;
+
 import lombok.Getter;
 import lombok.ToString;
 import xyz.sluggard.transmatch.core.Engine;
@@ -15,6 +17,9 @@ public class CancelEvent extends EngineEvent {
 	
 	public CancelEvent(Order order, Engine source) {
 		super(source);
+		if(order.isMarket()) {
+			order.setPrice(BigDecimal.ZERO);
+		}
 		order.negate();
 		this.order = order;
 	}
