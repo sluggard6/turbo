95567326cfc8f7936e07352d2c81a40993b9a593
fang <sydra@163.com>
2019-11-19 11:35:16 +0800

    增加撮合压力测试 修复bug

---
+++ src/main/java/xyz/sluggard/transmatch/exception/ExistedIdException.java
@@ -0,0 +1,30 @@
+package xyz.sluggard.transmatch.exception;
+
+public class ExistedIdException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ExistedIdException() {
+		super();
+	}
+
+	public ExistedIdException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public ExistedIdException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ExistedIdException(String message) {
+		super(message);
+	}
+
+	public ExistedIdException(Throwable cause) {
+		super(cause);
+	}
+
+}
